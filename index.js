4e6d045944bc6c99b4f9f2eb111f58976d7cb735
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2023-07-25 23:09:38 +0530

    working prototype

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let Urls = []
+let Urls = ['dsssfs','asfsfas','asffsdaww']
 const saved = document.querySelector('#save-btn')
 const url = document.querySelector('#input-box')
 const listU = document.querySelector('#list')
@@ -6,10 +6,16 @@ const listU = document.querySelector('#list')
 
 saved.addEventListener("click", function(){
     Urls.push(url.value)
-    console.log(Urls)
+    url.value=""
+    renderUrls()
+})
+
+function renderUrls(){
     let ListUrls = ""
     for(let i = 0;i <Urls.length; i++){
-        ListUrls += "<li>" + Urls[i] + "</li>"
+        ListUrls += `<li> <a href='${Urls[i]}' target='_blank'> 
+        ${Urls[i]}
+        </a> </li>`
     }
     listU.innerHTML = ListUrls
-})
+}
