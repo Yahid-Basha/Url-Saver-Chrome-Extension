905ad5ad0304a8152d962c349f1ac7b18725d294
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2023-07-25 21:50:26 +0530

    working list reneder

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+let Urls = []
+const saved = document.querySelector('#save-btn')
+const url = document.querySelector('#input-box')
+const listU = document.querySelector('#list')
 
-function save(){
-    console.log("selected")
-}
+
+saved.addEventListener("click", function(){
+    Urls.push(url.value)
+    console.log(Urls)
+    let ListUrls = ""
+    for(let i = 0;i <Urls.length; i++){
+        ListUrls += "<li>" + Urls[i] + "</li>"
+    }
+    listU.innerHTML = ListUrls
+})
