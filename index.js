fed2ab5bc50682d93a776a49c72098b44362f333
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2023-07-25 23:58:28 +0530

    clear btn works

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-let Urls = ['dsssfs','asfsfas','asffsdaww']
+let Urls = []
 const saved = document.querySelector('#save-btn')
 const url = document.querySelector('#input-box')
 const listU = document.querySelector('#list')
+const clear = document.querySelector('#clear-btn')
+const savedUrls = JSON.parse(localStorage.getItem('ListUrls'))
 
+if(savedUrls){
+    Urls = savedUrls
+    renderUrls()
+}
+
+clear.addEventListener('dblclick',function(){
+    localStorage.clear()
+    Urls = []
+    renderUrls()
+})
 
 saved.addEventListener("click", function(){
     Urls.push(url.value)
+    localStorage.setItem('ListUrls',JSON.stringify(Urls))
     url.value=""
     renderUrls()
 })
